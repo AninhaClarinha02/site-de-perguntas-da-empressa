e7a248a155b7dd85c690b0ff156e959ebfbfac61
AninhaClarinha02 <00001094279043sp@al.educacao.sp.gov.br>
2024-11-13 14:48:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Entrega',
+    'Quanto é a taxa de entrega?',
+    'O valor da entrega varia pela bebida e sua qualidade variandao entre 15-25 reais por quilômetro'
+)
+
+criaCartao(
+    'Qualidade',
+    'A alta qualidade das bebidas tem garantia?',
+    'Sim nossa empressa garante as bebidas de mais alta qualidade, sendo comprovado pela anvisa '
+)
+
+criaCartao(
+    'Localização',
+    'Onde se localiza a adega Terraço das bebidas?',
+    'Nossa adega está localizada na região da zona sul (bairro Americanópolis) Rua Giovanni Mário 262'
+)
+
+criaCartao(
+    'Desconto',
+    'Nossa empressa faz desconto em venda de alta quantidade?',
+    'NÃO, nossa empressa não faz desconto por quantidade de venda'
+)
